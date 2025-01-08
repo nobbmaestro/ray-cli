31c9540d06412d66af97d8e32038f4fdc9972f66
Norbert Batiuk <norbert@batiuk.se>
2025-01-08 22:41:24 +0100

    Improve timing accuracy to prevent drift over durations

--- ray_cli/app.py
+++ ray_cli/app.py
@@ -1,10 +1,41 @@
-import itertools
 import time
-from typing import Optional
+from typing import Generator, Optional
 
 from ray_cli.dispatchers import SACNDispatcher
 from ray_cli.modes import DmxDataGenerator
 from ray_cli.utils import Feedback, ProgressBar, TableLogger
+
+
+class Throttle:
+    def __init__(self, rate: int):
+        self._rate = rate if rate >= 0 else 1
+        self.last_tick = time.perf_counter()
+
+    @property
+    def rate(self) -> int:
+        return self._rate
+
+    @property
+    def time_step(self) -> float:
+        return 1 / self._rate
+
+    def wait_next(self):
+        target_tick = self.last_tick + self.time_step
+        now = time.perf_counter()
+
+        if now < target_tick:
+            time_to_sleep = target_tick - now
+            time.sleep(time_to_sleep)
+
+        self.last_tick = target_tick
+
+    def loop(self, duration: Optional[int] = None) -> Generator[int, None, None]:
+        ticks = 0
+        max_ticks = (self.rate * duration) if duration else None
+        while max_ticks is None or ticks < max_ticks:
+            self.wait_next()
+            yield ticks
+            ticks += 1
 
 
 class App:
@@ -21,9 +52,10 @@ class App:
         self.channels = channels
         self.fps = fps
         self.duration = duration
+        self.throttle = Throttle(fps)
 
         self.table_logger = TableLogger(channels)
-        self.progress_bar = ProgressBar(round(fps * duration) if duration else None)
+        self.progress_bar = ProgressBar((fps * duration) if duration else None)
 
     def purge_output(self):
         self.dispatcher.send([0 for _ in range(self.channels)])
@@ -36,17 +68,9 @@ class App:
         self.dispatcher.start()
 
         t_start = time.time()
-
-        num_frames = (
-            range(round(self.fps * self.duration))
-            if self.duration
-            else itertools.count(0, 1)
-        )
-
-        for i in num_frames:
-            t_0 = time.perf_counter()
-
+        for i in self.throttle.loop(self.duration):
             payload = next(self.generator)
+
             if not dry:
                 self.dispatcher.send(payload)
 
@@ -54,14 +78,7 @@ class App:
                 self.table_logger.report(i + 1, payload)
 
             elif feedback == Feedback.PROGRESS_BAR:
-                self.progress_bar.report(
-                    i + 1,
-                    time.time() - t_start,
-                )
-
-            elapsed_time = time.perf_counter() - t_0
-            t_sleep = max(0, 1 / self.fps - elapsed_time)
-            time.sleep(t_sleep)
+                self.progress_bar.report(i + 1, time.time() - t_start)
 
         if not dry:
             self.purge_output()
